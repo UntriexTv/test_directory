04f55c2339da5ae76861ee4ba64934a9781baace
Untriex Programming <48880229+UntriexTv@users.noreply.github.com>
2021-05-07 19:05:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,19 +4,7 @@ from hypercorn.config import Config
 from hypercorn.asyncio import serve
 from main import app
 
-import socket
-
-
-def get_my_ip_address(remote_server="google.com"):
-    """
-    Return the/a network-facing IP number for this system.
-    """
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect((remote_server, 80))
-        return s.getsockname()[0]
-
-
 config = Config()
-config.bind = [f"""{get_my_ip_address(remote_server="192.168.1.1")}:8000"""]
+config.bind = ["0.0.0.0:8000"]
 config.use_reloader = True
 asyncio.run(serve(app, config))
