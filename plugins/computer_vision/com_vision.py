999dbcd2d2c38382f8cf5f95b74886f565cf90a1
Untriex Programming <48880229+UntriexTv@users.noreply.github.com>
2021-05-08 16:17:21 +0200

    Add files via upload

--- plugins/computer_vision/com_vision.py
+++ plugins/computer_vision/com_vision.py
@@ -1,6 +1,10 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
 import imutils
 import numpy as np
+import requests
 
 protopath = "MobileNetSSD_deploy.prototxt"
 modelpath = "MobileNetSSD_deploy.caffemodel"
@@ -12,33 +16,30 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
            "sofa", "train", "tvmonitor"]
 
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+# allow the camera to warmup
+time.sleep(0.1)
+# grab an image from the camera
+while True:
+    camera.capture(rawCapture, format="bgr")
+    image = rawCapture.array
 
-def main():
-    global person_counter
-    cap = cv2.VideoCapture(0)
+    image = imutils.resize(image, width=1024, height=1024)
+    (H, W) = image.shape[:2]
 
-    while True:
-        ret, frame = cap.read()
-        frame = imutils.resize(frame, width=600, height=600)
+    blob = cv2.dnn.blobFromImage(image, 0.007843, (W, H), 127.5)
 
-        (H, W) = frame.shape[:2]
+    detector.setInput(blob)
+    person_detections = detector.forward()
 
-        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+    for i in np.arange(0, person_detections.shape[2]):
+        confidence = person_detections[0, 0, i, 2]
+        if confidence > 0.2:
+            idx = int(person_detections[0, 0, i, 1])
 
-        detector.setInput(blob)
-        person_detections = detector.forward()
-
-        for i in np.arange(0, person_detections.shape[2]):
-            confidence = person_detections[0, 0, i, 2]
-            if confidence > 0.5:
-                idx = int(person_detections[0, 0, i, 1])
-
-                if CLASSES[idx] == "person":
-                    person_counter += 1
-
-        print(person_counter)
-        person_counter = 0
-        cv2.waitKey(2000)
-
-
-main()
+            if CLASSES[idx] == "person":
+                person_counter += 1
+    r = requests.post("http://127.0.0.1:8000/update_sensor", json={"name": "pocet ludi", "value": str(person_counter)})
+    time.sleep(60)
