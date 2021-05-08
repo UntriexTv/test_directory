38263f8f3e5c7d4e668738ee35391f052e2a909c
Untriex Programming <48880229+UntriexTv@users.noreply.github.com>
2021-05-08 16:06:10 +0200

    Add files via upload

---
+++ plugins/computer_vision/com_vision.py
@@ -0,0 +1,44 @@
+import cv2
+import imutils
+import numpy as np
+
+protopath = "MobileNetSSD_deploy.prototxt"
+modelpath = "MobileNetSSD_deploy.caffemodel"
+detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
+person_counter = 0
+
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+
+
+def main():
+    global person_counter
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        frame = imutils.resize(frame, width=600, height=600)
+
+        (H, W) = frame.shape[:2]
+
+        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+
+        detector.setInput(blob)
+        person_detections = detector.forward()
+
+        for i in np.arange(0, person_detections.shape[2]):
+            confidence = person_detections[0, 0, i, 2]
+            if confidence > 0.5:
+                idx = int(person_detections[0, 0, i, 1])
+
+                if CLASSES[idx] == "person":
+                    person_counter += 1
+
+        print(person_counter)
+        person_counter = 0
+        cv2.waitKey(2000)
+
+
+main()
