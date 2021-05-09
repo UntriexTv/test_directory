4089ba0579555bf755567521945d000c05ead01f
Untriex Programming <48880229+UntriexTv@users.noreply.github.com>
2021-05-09 14:47:31 +0200

    Add files via upload

--- plugins/computer_vision/com_vision.py
+++ plugins/computer_vision/com_vision.py
@@ -1,10 +1,11 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+#! /usr/bin/python3
+import io
 import time
+
+import picamera
 import cv2
-import imutils
-import numpy as np
 import requests
+import numpy as np
 
 protopath = "MobileNetSSD_deploy.prototxt"
 modelpath = "MobileNetSSD_deploy.caffemodel"
@@ -16,17 +17,25 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
            "sofa", "train", "tvmonitor"]
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
-# allow the camera to warmup
-time.sleep(0.1)
-# grab an image from the camera
-while True:
-    camera.capture(rawCapture, format="bgr")
-    image = rawCapture.array
+# Create a memory stream so photos doesn't need to be saved in a file
+stream = io.BytesIO()
 
-    image = imutils.resize(image, width=1024, height=1024)
+# Get the picture (low resolution, so it should be quite fast)
+# Here you can also specify other parameters (e.g.:rotate the image)
+while True:
+    with picamera.PiCamera() as camera:
+        camera.resolution = (320, 240)
+        camera.capture(stream, format='jpeg')
+
+    # Convert the picture into a numpy array
+    buff = np.frombuffer(stream.getvalue(), dtype=np.uint8)
+
+    # Now creates an OpenCV image
+    image = cv2.imdecode(buff, 1)
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
     (H, W) = image.shape[:2]
 
     blob = cv2.dnn.blobFromImage(image, 0.007843, (W, H), 127.5)
@@ -41,5 +50,6 @@ while True:
 
             if CLASSES[idx] == "person":
                 person_counter += 1
-    r = requests.post("http://127.0.0.1:8000/update_sensor", json={"name": "pocet ludi", "value": str(person_counter)})
-    time.sleep(60)
+
+    r = requests.post("http://127.0.0.1:8000/update_sensor", json={"name": "Pocet ludi", "value": str(person_counter)})
+    time.sleep(30)
